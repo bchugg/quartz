3a4bdaf2a463e986481d8955a863f36f4d19fc15
Ben Chugg <ben.ih.chugg@gmail.com>
2024-10-04 11:15:24 -0400

    fix suffix issue

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "The Stats Map",
-    pageTitleSuffix: "Suffix",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
