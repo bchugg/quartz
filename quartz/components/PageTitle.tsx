5d1ec220857bf7a3f0227c69e6eb933bce8973ac
Ben Chugg <ben.ih.chugg@gmail.com>
2024-10-26 17:29:37 -0400

    add logo

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -7,17 +7,44 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
+    <div class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>
+      <img src={"/static/network.jpeg"} class='title-image'/>
+      <h2>{title}</h2>
+    </a>
+    {/* <h2 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>{title}</a>
-    </h2>
+    </h2> */}
+    </div>
   )
 }
 
 PageTitle.css = `
-.page-title {
-  font-size: 1.75rem;
-  margin: 0;
+.title-image {
+  border-radius: 50%;
+  margin-top: 0;
+  opacity: 0.8;
+  width: 40%;
+  @media all and (max-width: 1200px) {
+    width: 20%;
+  }
 }
+.page-title:hover {
+  opacity: 0.6; /* Reduces opacity on hover */
+  transition: opacity 0.3s ease-in-out; /* Smooth transition */
+}
+.page-title {
+  margin: 0;
+  text-align: center;
+  width: 100%
+  display: block;
+  a {
+    h2 {
+      margin: 0;
+      color: var(--secondary);  
+    }
+  }
+} 
 `
 
 export default (() => PageTitle) satisfies QuartzComponentConstructor
